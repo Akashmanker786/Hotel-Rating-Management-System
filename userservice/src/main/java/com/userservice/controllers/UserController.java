5b9f44b2842ad47a5c54fa7214de7bdc850ec293
Akashmanker786 <akashmanker722@gmail.com>
2025-03-30 23:40:31 +0530

    implemented config server & circuit breaker,rateLimiter,retry module in the project

--- userservice/src/main/java/com/userservice/controllers/UserController.java
+++ userservice/src/main/java/com/userservice/controllers/UserController.java
@@ -2,6 +2,11 @@ package com.userservice.controllers;
 import com.userservice.entity.User;
 import com.userservice.payload.ApiResponse;
 import com.userservice.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +17,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
@@ -26,10 +33,24 @@ public class UserController {
         return new ResponseEntity<>(userService.getAll(),HttpStatus.FOUND);
     }
 
+    int retryCount=1;
+
     @GetMapping("/{id}")
+//    @CircuitBreaker(name = "ratingHotelCircuitBreaker" , fallbackMethod = "ratingHotelServiceFallback")
+//    @Retry(name = "ratingHotelCircuitRetry",fallbackMethod = "ratingHotelServiceFallback")
+    @RateLimiter(name = "ratingHotelRateLimiter",fallbackMethod = "ratingHotelServiceFallback")
     public ResponseEntity<User> getById(@PathVariable("id") String id){
+        log.info("Api Called and Retry Count is = {}",retryCount++);
         return new ResponseEntity<>(userService.getById(id),HttpStatus.FOUND);
     }
+
+//    fallback method called if the rating or hotel service is either down
+    public ResponseEntity<User> ratingHotelServiceFallback(String id , Exception e) {
+        log.info("FALLBACK METHOD CALLED BECAUSE EIHTER RATING OR HOTEL SERVICE IS DOWN...",e.getMessage());
+        User user = User.builder().userId("dummyId").userAbout("dummyAbout").userEmail("dummyEmail").userName("dummyName").build();
+        return new ResponseEntity<User>(user,HttpStatus.SERVICE_UNAVAILABLE);
+    }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponse> deleteUser(@PathVariable("id") String id){
